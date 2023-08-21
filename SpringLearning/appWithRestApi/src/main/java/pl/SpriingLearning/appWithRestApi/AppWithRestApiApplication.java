0b56493bc1e7d46b2828e05b32028536cad0b853
Kierznowski <przemek.kierznowski@gmail.com>
2023-08-21 22:35:01 +0200

    Adding First Spring Hello App
    First App in Spring with Rest Endpoint

---
+++ SpringLearning/appWithRestApi/src/main/java/pl/SpriingLearning/appWithRestApi/AppWithRestApiApplication.java
@@ -0,0 +1,13 @@
+package pl.SpriingLearning.appWithRestApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWithRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWithRestApiApplication.class, args);
+	}
+
+}
