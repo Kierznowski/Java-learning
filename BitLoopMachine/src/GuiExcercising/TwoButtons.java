80a89070a87261c0178201c844994d2deaf5457f
Kierznowski <przemek.kierznowski@gmail.com>
2023-08-25 20:18:27 +0200

    Java Swing Excercising
    Learning on Gui Swing and inner-outer classes. How to animate objects in javax.swing.

---
+++ BitLoopMachine/src/GuiExcercising/TwoButtons.java
@@ -0,0 +1,39 @@
+package GuiExcercising;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TwoButtons {
+    private JFrame frame;
+    private JLabel label;
+
+    public static void main(String[] args) {
+        TwoButtons gui = new TwoButtons();
+        gui.go();
+    }
+
+    public void go() {
+        frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JButton labelButton = new JButton("Change label");
+        labelButton.addActionListener(event -> label.setText("Ouch!"));
+
+        JButton colorButton = new JButton(("Change circle"));
+        colorButton.addActionListener(event -> frame.repaint());
+
+        label = new JLabel("Here is label");
+        MyDrawPanel drawPanel = new MyDrawPanel();
+
+
+        frame.getContentPane().add(BorderLayout.SOUTH, colorButton);
+        frame.getContentPane().add(BorderLayout.CENTER, drawPanel);
+        frame.getContentPane().add(BorderLayout.EAST, labelButton);
+        frame.getContentPane().add(BorderLayout.WEST, label);
+
+        frame.setSize(500, 400);
+        frame.setVisible(true);
+    }
+}
